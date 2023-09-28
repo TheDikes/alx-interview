e928a2035cc33ddc43000302886917610de5d9fe
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-09-29 00:44:51 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,12 +4,13 @@ A function returning the perimeter of an island in grid
 """
 def island_perimeter(grid):
     """
-    a list of list of int:
-    0 rep water
-    1 rep land
-    Each cell is square, with a side length of 1
-    Cells are connected horizontally/vertically
-    grid is rectangular and completely surrounded by water
+    A function returning the perimeter of an island in grid.
+
+    Args:
+    grid (List[List[int]]): A list of lists representing the grid with 0s for water and 1s for land.
+
+    Returns:
+    int: The perimter of the island.
     """
 
     if not grid or len(grid) == 0:
@@ -22,22 +23,22 @@ def island_perimeter(grid):
     for row in range(rows):
         for cols in range(cols):
             if grid[row][col] == 1:
-                """ count the top boundary """
+                # count the top boundary 
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
 
-                """ count the left boundary """
+                # count the left boundary 
                 if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
 
-                """ count the right boundary """
+                # count the right boundary 
                 if col == cols - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
 
-                """ count the bottom boundary """
+                # count the bottom boundary 
                 if row == rows - 1 or grid[row + 1][col] == 0:
                     perimeter += 1
 
-        return perimeter
+    return perimeter
 
 
