ee068681c9b0fa9c606b5c7142f5eda36da97776
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-09-29 01:05:02 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -23,22 +23,16 @@ def island_perimeter(grid):
     for row in range(rows):
         for cols in range(cols):
             if grid[row][col] == 1:
-                # count the top boundary 
-                if row == 0 or grid[row - 1][col] == 0:
-                    perimeter += 1
+                 # For each land cell, check its adjacent cells
+                # and count the number of water cells (0) in its surroundings
+                perimeter += 4  # Each land cell contributes 4 to the perimeter
+               
+                # check the left boundary 
+               if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2  # If the left cell is also land, subtract 2
 
-                # count the left boundary 
-                if col == 0 or grid[row][col - 1] == 0:
-                    perimeter += 1
-
-                # count the right boundary 
-                if col == cols - 1 or grid[row][col + 1] == 0:
-                    perimeter += 1
-
-                # count the bottom boundary 
-                if row == rows - 1 or grid[row + 1][col] == 0:
-                    perimeter += 1
+                # Check the cell above
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
 
     return perimeter
-
-
